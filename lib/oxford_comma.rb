3d3723c79a82af6ac9f5f03545bca407cc3595f4
Ad Friedman <adien330@yahoo.com>
2019-03-06 21:58:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+# def oxford_comma(array)
+# if array.length == 2
+#  array.join(" and ")
+# elsif array.length  > 2
+#   array[0...array.last]
+# else
+#   array.join
+# end
+# end
 def oxford_comma(array)
-
+if array.length == 2
+ array.join(" and ")
+elsif array.length > 2
+  "#{array[0...-1].join(", ")}, and #{array[-1]}"
+else
+  array.join
+end
 end
